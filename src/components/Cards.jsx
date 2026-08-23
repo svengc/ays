e884931091114d48d0a4e1d9eab5d5b97c90c9ba
Stiven Giraldo <giraldostiven150@gmail.com>
2026-08-23 06:12:06 -0500

    Se organiza el icon de head

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,4 @@
-import bagShop from "../assets/icons/CartIcon.svg"
+import bagShop from "../assets/icons/CartIconDark.svg"
 
 function CardHome({ category, name, price, image }) {
     return (
@@ -17,17 +17,17 @@ function CardHome({ category, name, price, image }) {
                 {/* Información del producto */}
                 <div className="h-full flex flex-col justify-start items-start">
 
-                    <span className="uppercase text-[10px] text-gray-500 leading-4">
+                    <span className="uppercase text-[10px] text-dorado leading-4">
                         {category}
                     </span>
 
-                    <h2 className="text-base font-medium leading-5 mt-1">
+                    <h2 className="text-base font-medium leading-5 mt-1 text-salvia-dark">
                         {name}
                     </h2>
 
                     {/* Precio */}
                     <div className="mt-auto">
-                        <p className="text-[14px] font-semibold leading-5">
+                        <p className="text-[14px] font-semibold leading-5 text-taupe-dark">
                             {price}
                         </p>
                     </div>
